7f163e60b1289d79935b4ad487d3c7918fae0aec
ttoocs <ttoocs@gmail.com>
2018-08-23 19:46:31 -0600

    Added some documentation to my enroll script

--- enroll.py
+++ enroll.py
@@ -1,3 +1,11 @@
+#Orgional author: Scott Saunders, 2017.
+# I guess a BSD license... just do whatever, it's a double while loop in selenium.
+
+##Origonally used to brute-force enrollment into classes without waitlists.
+## Basically, given your username/password, it logins, and goes to current(or more probably, winter2018)'s class schedule, and enrolls with anything you have there. If somebody happens to have left the class, you now get in!
+
+#Note: Most of the ID's for find_element_by_id() will probably change over time, but if you do the steps manually, and record the ID's via right-clicking them, insepct element, and finding the < id="foobar" >, you can update them as needed. (This happens every semester)
+
 import time
 import sys
 from selenium import webdriver
@@ -19,16 +27,18 @@ while True:
         
         #time.sleep(1)
         
+        #sign-in
         username=sys.argv[1]
         password=sys.argv[2]
         driver.find_element_by_id("eidtext").send_keys(username)
         driver.find_element_by_id("passwordtext").send_keys(password)
         driver.find_element_by_id("signinbutton").submit()
-        
+
         time.sleep(2)
         driver.get("https://csprd.my.ucalgary.ca/psc/csprd/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL")
         driver.find_element_by_id("DERIVED_SSS_SCR_SSS_LINK_ANCHOR2").click()
         
+        #While loop of enrolling classes.
         while True:
             #Internal Iframe of the resulting url.
             
